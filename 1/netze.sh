41b55011e7a2c0eeb2e357cc1dac7dee66ff5d41
Sam Crawford <sam.crawford@stud.plus.ac.at>
2025-03-11 13:33:03 +0100

    removed garbage

--- 1/netze.sh
+++
@@ -1,6 +0,0 @@
-sudo tcpdump --interface wlo1 -w dump.pcap &
-wget --delete-after http://student.cosy.sbg.ac.at/~scrawford/file
-sudo killall tcpdump
-wireshark dump.pcap
-# tshark -r dump.pcap -Y "tcp" -T fields -e frame.time_relative -e tcp.seq >tcp_data.txt
-# python3 plot_tcp.py tcp_data.txt
--- 1/plot.gnu
+++
@@ -1,9 +0,0 @@
-set terminal pngcairo enhanced font 'Arial,12' size 1000,500
-set output 'output.png'
-
-set xlabel "Zeit (s)"
-set ylabel "Sequenznummern"
-set title "TCP Sequence Number Over Time"
-set grid
-
-plot "dump.txt" using 1:2 linestyle 1 title "TCP Sequence"
